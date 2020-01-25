2fb0008d2a6d9cd8ea7d8b0432ae89be8ba7975d
ManarArabi <manar.araby.ma@gmail.com>
2020-01-25 16:54:40 +0200

    changing intro, handling valid format

---
+++ Client_side/src/app/result/result.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NumberService, NumberDetails } from './../numbers.service';
+import { AppComponent } from './../app.component';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+export class ResultComponent implements OnInit {
+
+  numberDetails: NumberDetails;
+  valid : string;
+
+  constructor(private ns:NumberService, private appc:AppComponent) { 
+    this.appc.changeIntro("Mobile Number Verfication");
+  }
+  ngOnInit() {
+    this.numberDetails = this.ns.getNumberDetails();
+
+    this.numberDetails.valid ? this.valid = "Yes" : this.valid = "No";
+
+    if (!this.numberDetails.location) this.numberDetails.location = "-"
+  }
+
+}
